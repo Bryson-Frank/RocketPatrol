c3ccf9c7a286052465ad52c2ce85535968ebe269
Bryson Frank <bmfrank@ucsc.edu>
2021-06-27 17:28:17 -0700

    wotking reset

--- Source/scenes/Play.js
+++ Source/scenes/Play.js
@@ -50,19 +50,27 @@ class Play extends Phaser.Scene {
         }
         this.scoreLeft = this.add.text(borderPadding + borderUISize, borderUISize + borderPadding * 2, this.p1Score, scoreConfig);
 
+        this.gameOver = false
+
         //play clock
         scoreConfig.fixedWidth = 0,
-        this.clock = this.time.delayedCall(60000, () => {
+        this.clock = this.time.delayedCall(1000, () => { //HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH
             this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER', scoreConfig).setOrigin(0.5);
             this.add.text(game.config.width/2, game.config.height/2 + 64, 'Press (r) to Resart', scoreConfig).setOrigin(0.5);
+            this.gameOver = true;
         }, null, this);
     }
     update() {
+        if(this.gameOver && Phaser.Input.Keyboard.JustDown(keyR)){
+            this.scene.restart();
+        }
         this.starfield.tilePositionX -= 4;
-        this.p1Rocket.update();
-        this.ship01.update();
-        this.ship02.update();
-        this.ship03.update();
+        if (!this.gameOver) {
+            this.p1Rocket.update();
+            this.ship01.update();
+            this.ship02.update();
+            this.ship03.update();
+        }
 
         //check collision
         if (this.checkCollision(this.p1Rocket, this.ship03)) {
