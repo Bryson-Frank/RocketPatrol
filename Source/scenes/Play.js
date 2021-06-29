d63e1f1b037cdcb7173cf6df0d003a1fa666014f
Bryson Frank <bmfrank@ucsc.edu>
2021-06-29 02:00:21 -0700

    image capitalization

--- Source/scenes/Play.js
+++ Source/scenes/Play.js
@@ -3,10 +3,10 @@ class Play extends Phaser.Scene {
         super("playScene");
     }
     preload() {
-        this.load.image('rocket', './assets/rocket.png');
-        this.load.image('spaceship', './assets/spaceship.png');
-        this.load.image('starfield', './assets/starfield.png');
-        this.load.spritesheet('explosion', './assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
+        this.load.image('rocket', './Assets/rocket.png');
+        this.load.image('spaceship', './Assets/spaceship.png');
+        this.load.image('starfield', './Assets/starfield.png');
+        this.load.spritesheet('explosion', './Assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
     }
     
     create() {
